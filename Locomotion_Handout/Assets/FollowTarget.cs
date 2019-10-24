ec705de57d66e20d713515b580608f0e1e6bfdb9
Carles Homs <36209557+ch0m5@users.noreply.github.com>
2019-10-24 14:31:39 +0200

    Animation Handout

---
+++ Locomotion_Handout/Assets/FollowTarget.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowTarget : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Camera camera;
+    Animator anim;
+    public GameObject target;
+    private RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        camera = GetComponent<Camera>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("move", agent.SetDestination(target.transform.position));
+
+        anim.SetFloat("vel x", agent.velocity.x);
+        anim.SetFloat("vel y", agent.velocity.z);
+    }
+}
