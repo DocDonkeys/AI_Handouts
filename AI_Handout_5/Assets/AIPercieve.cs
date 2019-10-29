bfec72e2655f9c90c0d42fa7ef8c6812913236ee
Carles Homs <36209557+ch0m5@users.noreply.github.com>
2019-10-29 18:41:41 +0100

    Handout 5 Progress

--- AI_Handout_5/Assets/AIPercieve.cs
+++ AI_Handout_5/Assets/AIPercieve.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class AIPercieve : MonoBehaviour
 {
-    public LayerMask mask;
+    public LayerMask targets;
+    public LayerMask obstacles;
 
     [Header("Area")]
     public bool area = true;
@@ -14,8 +15,11 @@ public class AIPercieve : MonoBehaviour
     public bool cone = true;
     public Camera vision;
 
+    private float visionDistance;
+    private float colliderSearchRadius;
+
+    private List<GameObject> detected;
     private Collider myCollider;
-    public float visionDistance;
 
     // Start is called before the first frame update
     void Start()
@@ -24,31 +28,57 @@ public class AIPercieve : MonoBehaviour
         vision = GetComponent<Camera>();
 
         visionDistance = vision.farClipPlane / Mathf.Cos(vision.fieldOfView / 2.0f);  // Make cone radius out of FOV angle and farClipPlane distance from camera
+
+        if (visionDistance > radius)    // Use the largest distance from the two perceptions (Area/Vision) for collider detection
+            colliderSearchRadius = visionDistance;
+        else
+            colliderSearchRadius = radius;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Collider[] colliders = Physics.OverlapSphere(transform.position, visionDistance, mask);
-
+        Collider[] colliders = Physics.OverlapSphere(transform.position, colliderSearchRadius, targets);
         Plane[] planes = GeometryUtility.CalculateFrustumPlanes(vision);
 
         for (int i = 0; i < colliders.Length; i++)
         {
-            bool ret = false;
-
-            if (colliders[i] != myCollider)
+            if (colliders[i] != myCollider) // If collider isn't from the agent who's scanning
             {
-                if (area && Vector3.Magnitude(colliders[i].transform.position - transform.position) < radius)
-                    ret = true;
+                PerceptionEvent eventData = null;
+                Vector3 targetDistance = colliders[i].transform.position - transform.position;
 
-                if (cone && GeometryUtility.TestPlanesAABB(planes, colliders[i].bounds))
-                    ret = true;
-            }
+                if (cone && GeometryUtility.TestPlanesAABB(planes, colliders[i].bounds))    // If sound detection ON and inside detection radius
+                {
+                    eventData.sense = PerceptionEvent.senses.VISION;
+                }
+                else if (area && targetDistance.magnitude < radius) // If visual detection ON and inside camera FOV
+                {
+                    eventData.sense = PerceptionEvent.senses.SOUND;
+                }
+                {
+                    if (!Physics.Raycast(transform.position, targetDistance, targetDistance.magnitude, obstacles))  // If no obstacles in the way
+                    {
+                        bool already_seen = false;
 
-            if (ret)
-            {
-                Debug.Log("EnemySighted!");
+                        for (int j = 0; j < detected.Count; j++)
+                        {
+                            if (detected[i] == colliders[i].gameObject)
+                            {
+                                already_seen = true;
+                                break;
+                            }
+                        }
+
+                        if (!already_seen)
+                        {
+                            //eventData.go = colliders[i].gameObject;
+                            //eventData.type = PerceptionEvent.types.NEW;
+
+                            //SendMessage(PerceptionEvent, eventData);
+                        }
+                    }
+                }
             }
         }
     }
