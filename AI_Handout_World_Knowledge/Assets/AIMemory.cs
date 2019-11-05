2d164d52db06b8c9b37d9d84b2ec1dc3ab9a1e02
Carles Homs <36209557+ch0m5@users.noreply.github.com>
2019-11-05 12:48:36 +0100

    Handout World_Knowledge Mostly Finished

--- AI_Handout_World_Knowledge/Assets/AIMemory.cs
+++ AI_Handout_World_Knowledge/Assets/AIMemory.cs
@@ -6,30 +6,47 @@ using System.Collections.Generic;
 // TODO 1: Create a simple class to contain one entry in the blackboard
 // should at least contain the gameobject, position, timestamp and a bool
 // to know if it is in the past memory
+public class blackboard_item
+{
+    public blackboard_item(GameObject obj_, Vector3 position_, float timestamp_)
+    {
+        obj = obj_;
+        position = position_;
+        timestamp = timestamp_;
+        in_memory = false;
+    }
+
+    public GameObject obj;
+    public Vector3 position;
+    public float timestamp;
+    public bool in_memory;
+}
 
 public class AIMemory : MonoBehaviour {
 
 	public GameObject Cube;
 	public Text Output;
+    public Dictionary<string, blackboard_item> memory;
 
-	// TODO 2: Declare and allocate a dictionary with a string as a key and
-	// your previous class as value
+    // Use this for initialization
+    void Start ()
+    {
+        // TODO 2: Declare and allocate a dictionary with a string as a key and
+        // your previous class as value
+        memory = new Dictionary<string, blackboard_item>();
+    }
 
-
-	// TODO 3: Capture perception events and add an entry if the player is detected
-	// if the player stop from being seen, the entry should be "in the past memory"
-
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () 
+// Update is called once per frame
+void Update () 
 	{
-		// TODO 4: Add text output to the bottom-left panel with the information
-		// of the elements in the Knowledge base
+        // TODO 3: Capture perception events and add an entry if the player is detected
+        // if the player stop from being seen, the entry should be "in the past memory"
 
-		Output.text = "< -- database display -- >";
+
+        // TODO 4: Add text output to the bottom-left panel with the information
+        // of the elements in the Knowledge base
+
+        Output.text = "< -- database display -- >";
 	}
 
 }
--- AI_Handout_World_Knowledge/Assets/AIPerceptionManager.cs
+++ AI_Handout_World_Knowledge/Assets/AIPerceptionManager.cs
@@ -1,22 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class AIPerceptionManager : MonoBehaviour {
 
 	public GameObject Alert;
+    public AIMemory agent;
 
-	// Update is called once per frame
-	void PerceptionEvent (PerceptionEvent ev) {
+    private void Start()
+    {
+        agent = GetComponent<AIMemory>();
+    }
+
+    // Update is called once per frame
+    void PerceptionEvent (PerceptionEvent ev) {
 
 		if(ev.type == global::PerceptionEvent.types.NEW)
 		{
 			Debug.Log("Saw something NEW");
 			Alert.SetActive(true);
-		}
+
+            if (agent.memory.ContainsKey(ev.go.name))
+            {
+                foreach (KeyValuePair<string, blackboard_item> entry in agent.memory)
+                {
+                    if (entry.Key == ev.go.name)
+                    {
+                        entry.Value.obj = ev.go;
+                        entry.Value.position = ev.go.transform.position;
+                        entry.Value.in_memory = false;
+                        entry.Value.timestamp = Time.time;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                agent.memory.Add(ev.go.name, new blackboard_item(ev.go, ev.go.transform.position, Time.time);
+            }
+
+        }
 		else
 		{
 			Debug.Log("LOST something");
 			Alert.SetActive(false);
-		}
+
+            foreach (KeyValuePair<string, blackboard_item> entry in agent.memory)
+            {
+                if (entry.Key == ev.go.name)
+                {
+                    entry.Value.in_memory = true;
+                    break;
+                }
+            }
+        }
 	}
 }
